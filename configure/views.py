c099ece6cfd735ce07aea056aac79c7fbe666244
yra3 <64928855+yra3@users.noreply.github.com>
2021-04-28 05:30:14 +0500

    start work with algorithm. Relialise simple algorithm перебором

--- configure/views.py
+++ configure/views.py
@@ -1,3 +1,52 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from configure.models import *
 
-# Create your views here.
+
+def summary_price(configure:list):
+    sum = 0
+    for bit in configure:
+        sum += bit.price
+    return sum
+
+
+def remove_over_budget(configurates:list, budget):
+    for configure in configurates:
+        if summary_price(configure) > budget:
+            configurates.remove(configure)
+# budget: int, priorities: list
+
+
+def find_configure(r):  # Ммм, хуита
+    budget = 10000
+    cpus = CPU.objects.filter(price__lt=budget)
+    gpus = GPU.objects.filter(price__lt=budget)
+    mothers = motherboard.objects.filter(price__lt=budget)
+    cgs = [[c, g] for c in cpus for g in gpus]
+    print(len(cgs))
+
+    remove_over_budget(cgs, budget)
+
+
+    cgs.sort(key=lambda x: summary_price(x))
+    cgpumother = [cg+[mother] for cg in cgs for mother in mothers]
+    print(len(cgs))
+
+    print(len(cgpumother))
+    remove_over_budget(cgpumother, budget)
+    print(len(cgpumother))
+    for cgm in cgpumother:
+        if cgm[0].socket != cgm[2].socket:
+            cgpumother.remove(cgm)
+    print(len(cgpumother))
+
+    for cgm in cgpumother:
+        if cgm[0].socket != cgm[2].socket:
+            cgpumother.remove(cgm)
+
+    return HttpResponse(cgs)
+
+
+
+if __name__=='__main__':
+    find_configure(10000)
--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from homepage import views
+from configure.views import find_configure
 urlpatterns = [
+    path('c', find_configure),
     path('', views.index),
     path('auto/', views.auto),
+
 ]
 
 
