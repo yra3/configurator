96f0c774abc6d1c4a8859233544c2c98caab37d9
yra3 <64928855+yra3@users.noreply.github.com>
2021-05-05 09:22:06 +0500

    set regression func

--- configure/views.py
+++ configure/views.py
@@ -23,9 +23,22 @@ budget_constraints = {
 }
 
 
-def priority_calc(type: int, budget: int):
-    return {
-        "1": lambda x, y: {
+class RegressionConfigurePrioritiesCalculator:
+    def __init__(self, budget: int, purpose_of_computer):
+        self.budget = budget
+        self.purpose_of_computer = purpose_of_computer
+
+    def get_priorities(self):
+        ctype = self.purpose_of_computer
+        if ctype == '1' or ctype == 1 or ctype == 'home':
+            return self.get_home_priorities()  # TODO write this priorities function
+        if ctype == '2' or ctype == 2 or ctype == 'game':
+            return self.get_game_priorities()
+        if ctype == '3' or ctype == 3 or ctype == 'work':
+            return self.get_work_priorities()  # TODO write this priorities function
+
+    def get_home_priorities(self):
+        return {
             'CPU': 0.25,
             'GPU': 0.2,
             'motherboard': 0.1,
@@ -34,18 +47,25 @@ def priority_calc(type: int, budget: int):
             'hard_35': 0.1,
             'ssd': 0.1,
             'powersupply': 0.1
-        },
-        '2': lambda x, y: {
-            'CPU': 4.69532 * pow(10, -12) * y * y - 1.53484 * pow(10, -6) * y + 0.285049,
-            'GPU': 1.87181 * pow(10, -16) * pow(y, 3) - 8.86215 * pow(10, -11) * y * y + 0.000012088 * y + 0.0458876,
-            'motherboard': 2.39955 * pow(10, -12) * y * y - 8.70106 * pow(10, -7) * y + 0.153233,
-            'RAM': -3.09443 * pow(10, -12) * y * y + 1.04014 * pow(10, -6) * y + 0.00444359,
-            'cooler': -1.28803 * pow(10, -13) * y * y + 1.27392 * pow(10, -7) * y + 0.010703,
-            'hard_35': 1.72394 * pow(10, -12) * y * y - 4.76713 * pow(10, -7) * y + 0.0915476,
-            'ssd': 1.72394 * pow(10, -12) * y * y - 4.76713 * pow(10, -7) * y + 0.0915476,
-            'powersupply': 1.15148 * pow(10, -12) - 2.98272 * pow(10, -7) * y + 0.0642154,
-        },
-        '3': lambda x, y: {
+        }
+
+    def get_game_priorities(self):
+        x = self.budget
+        xx = x ^ 2
+        xxx = x ^ 3
+        return {
+            'CPU': 4.69532 * pow(10, -12) * xx - 1.53484 * pow(10, -6) * x + 0.285049,
+            'GPU': 1.87181 * pow(10, -16) * xxx - 8.86215 * pow(10, -11) * xx + 0.000012088 * x + 0.0458876,
+            'motherboard': 2.39955 * pow(10, -12) * xx - 8.70106 * pow(10, -7) * x + 0.153233,
+            'RAM': -3.09443 * pow(10, -12) * xx + 1.04014 * pow(10, -6) * x + 0.00444359,
+            'cooler': -1.28803 * pow(10, -13) * xx + 1.27392 * pow(10, -7) * x + 0.010703,
+            'hard_35': 1.72394 * pow(10, -12) * xx - 4.76713 * pow(10, -7) * x + 0.0915476,
+            'ssd': 1.72394 * pow(10, -12) * xx - 4.76713 * pow(10, -7) * x + 0.0915476,
+            'powersupply': 1.15148 * pow(10, -12) - 2.98272 * pow(10, -7) * x + 0.0642154,
+        }
+
+    def get_work_priorities(self):
+        return {
             'CPU': 0.45,
             'GPU': 0.15,
             'motherboard': 0.07,
@@ -54,9 +74,8 @@ def priority_calc(type: int, budget: int):
             'hard_35': 0.05,
             'ssd': 0.05,
             'powersupply': 0.05,
-        },
-    }.get(type, -1)
-    # TODO write priorities functions
+        }
+
 
 
 prioriti_calculators = {
@@ -93,6 +112,10 @@ prioriti_calculators = {
 }
 
 def autopage(request):
+    import time
+    tm = time.time()
+    time.sleep()
+    m2 = time.time()
     stroka = ''
     pri = int(request.POST['price'])
     tip = request.POST['answer']
@@ -127,8 +150,8 @@ def autopage(request):
 
 
 def real_auto_configure(budget: int, configure_type: int, hdd_ssd=2, is_banchmarck_mode=0):
-    prioriti_calculator = prioriti_calculators[configure_type]
-    priorities = prioriti_calculator(budget)
+    prioriti_calculator = RegressionConfigurePrioritiesCalculator(budget, configure_type)
+    priorities = prioriti_calculator.get_priorities()
     from configure.StrictConstraintMethod import StrictConstraintMethod
     hdd_ssd = 2
     finder = StrictConstraintMethod(budget, priorities, hdd_ssd)
