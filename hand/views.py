c25b1a0c6acc9a8f9d0fb022594207376ed9f31f
yra3 <https://github.com/yra3>
2021-06-15 12:54:44 +0500

    added loop for constraints in catalog.html

--- hand/views.py
+++ hand/views.py
@@ -48,7 +48,68 @@ def catalog_cpu(request):
     # , context=data
     cpus = CPU.objects.all()
     data = {
+        'values_list': [
+            [
+                'Количество ядер',
+                'cores_cpu[]',
+                'processor_manufacturer',
+                CPU.objects.values_list('number_of_cores', flat=True).distinct()
+            ],
+            [
+                'Производитель процессора',
+                'manufacturer_cpu[]',
+                'processor_socket',
+                ['AMD', 'Intel']
+            ],
+            [
+                'Сокет',
+                'socket_cpu[]',
+                'processor_cores',
+                CPU.objects.values_list('socket', flat=True).distinct()
+            ],
+            [
+                'Интегрированное графическое ядро',
+                'integrated_graphics_core_cpu[]',
+                'processor_integrated_graphics_core',
+                CPU.objects.values_list('integrated_graphics_core', flat=True).distinct()
+            ],
+            [
+                'Многопоточность',
+                'multithreaded_cpu[]',
+                'processor_multithreaded',
+                CPU.objects.values_list('multithreading', flat=True).distinct()
+            ],
+            [
+                'Год релиза',
+                'year_cpu[]',
+                'processor_year',
+                CPU.objects.values_list('release_year', flat=True).distinct()
+            ],
+            [
+                'Тип памяти',
+                'memory_type_cpu[]',
+                'processor_memory_type',
+                CPU.objects.values_list('memory_type', flat=True).distinct()
+            ],
+            [
+                'Базовая частота процессора (МГц)',
+                'frequency_cpu',
+                'processor_base_freq',
+                ['Все', 'Менее 3000', '3001-4000', '4001 и более']
+            ],
+            [
+                'Техпроцесс',
+                'techprocess_cpu[]',
+                'processor_techprocess',
+                CPU.objects.values_list('technical_process', flat=True).distinct()
+            ],
+            # [
+            #     'Год релиза',
+            #     'year_cpu[]',
+            #     CPU.objects.values_list('release_year', flat=True).distinct()
+            # ],
+        ],
         'cpus': cpus,
     }
-    response = render(request, template_name='configure/catalog2.html', context=data)
+    response = render(request, template_name='configure/catalog.html', context=data)
     return response
